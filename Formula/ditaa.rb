eb35101eba32e3010347d61766d0fe17eabcc624
TakesxiSximada <8707279+TakesxiSximada@users.noreply.github.com>
2022-01-09 15:23:32 +0900

    Add Janapese ditaa.

---
+++ Formula/ditaa.rb
@@ -0,0 +1,22 @@
+class Ditaa < Formula
+  desc "Convert ASCII diagrams into proper bitmap graphics with Chinese Janapese Korean Wide character support."
+  homepage "https://github.com/TakesxiSximada/ditaa"
+  url "https://github.com/TakesxiSximada/ditaa/releases/download/v0.11.0-symdon-01/ditaa-0.11.0-standalone.jar"
+  sha256 "9418aa63ff6d89c5d2318396f59836e120e75bea7a5930c4d34aa10fe7a196a9"
+  license "LGPL-3.0"
+  revision 1
+
+  depends_on "openjdk"
+
+  def install
+    libexec.install "ditaa-#{version}-standalone.jar"
+    (bin/"ditaa").write <<~EOS
+      #!/bin/bash
+      exec "#{Formula["openjdk"].opt_bin}/java" -jar "#{libexec}/ditaa-#{version}-standalone.jar" "$@"
+    EOS
+  end
+
+  test do
+    system "#{bin}/ditaa", "-help"
+  end
+end
